ae71f0b23ce0d0a6890a4defd3ebc549b2fe4cb2
Tom Hatcher <tom.hatcher@myob.com>
2020-12-15 09:13:49 +1300

    Initial project commit.

---
+++ MontyHallv2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontyHallv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
